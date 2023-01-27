d73ac5537713bcbe07686a49db2190ee2db0de14
Drumil_Akhenia <akheniad@gmail.com>
2023-01-27 07:53:54 +0530

    added comments in server.js

--- backend/server.js
+++ backend/server.js
@@ -1,33 +1,41 @@
 const express = require('express');
-const app = express();
 const connectDB = require('./config/db');
 const cors = require('cors');
 const dotenv = require('dotenv').config();
 const bodyParser = require('body-parser');
-const {errorHandler} = require('./middleware/errorMiddleware');
-const {userTypeHandler} = require('./middleware/userTypeMiddleware');
+const { errorHandler } = require('./middleware/errorMiddleware');
+const { userTypeHandler } = require('./middleware/userTypeMiddleware');
 
+// dotenv configt
+dotenv.config();
 
-const PORT = process.env.PORT || 3001;
+// rest object
+const app = express();
+
+// connect to database
 connectDB();
 
+// middleware
 app.use(bodyParser.json());
 app.use(cors());
 app.use(express.json());
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
+app.use('/', userTypeHandler);
 
-app.get('/', (req, res) => {
-    res.send('yo man');
-});
+// app.get('/', (req, res) => {
+//     res.send('yo man');
+// });
 
-// REASON FOR PUT userTypeHandler in comment is for now JWT token is not configured
 
-app.use('/',userTypeHandler);
-
-app.use('/api/user',require('./routes/userRoutes.js'));
-app.use('/api/owner',require('./routes/ownerRoutes.js'));
-app.use('/api/worker',require('./routes/workerRoutes.js'));
-app.use('/api/customer',require('./routes/customerRoutes.js'));
+// routes
+app.use('/api/user', require('./routes/userRoutes.js'));
+app.use('/api/owner', require('./routes/ownerRoutes.js'));
+app.use('/api/worker', require('./routes/workerRoutes.js'));
+app.use('/api/customer', require('./routes/customerRoutes.js'));
 app.use(errorHandler);
 
-app.listen(PORT,()=>{console.log('server is listening on PORT')})
+// port
+const PORT = process.env.PORT || 3001;
+
+// listening port
+app.listen(PORT, () => { console.log('server is listening on PORT') })
