ca0e63d8402827214d703fb36cd19b850aaac9fd
OmiKakadiya <omikakadiya7777@gmail.com>
2023-03-14 20:42:57 +0530

    fix_design

--- frontend/src/pages/guestUser/showCompanies/CompanyDetails.jsx
+++ frontend/src/pages/guestUser/showCompanies/CompanyDetails.jsx
@@ -6,50 +6,66 @@ function CompanyDetails({ redirectHandler, userType, company, index }) {
 
     const [companyViewModelState, setCompanyViewModelState] = useState(false);
     return (
-        <div key={index} className="container">
-            <label style={{ color: "#1a237e", fontSize: "1.5rem", fontWeight: "700" }}>
+        <table>
+            <tr>
+            <div key={index} className="container">
+             <td><label style={{ color: "#1a237e", fontSize: "1.5rem", fontWeight: "700",marginLeft:"19px" }}>
                 {company.name}
             </label>
+            </td>
+            
             <CompanyViewModel
                 show={companyViewModelState}
                 onHide={() => setCompanyViewModelState(false)}
                 data={company}
             />
-            <button className="btn btn-info ml-4 mt-1" style={{
-                fontSize: "1.2em", fontWeight: "700", color: "darkblue",
-            }} data-bs-toggle="button" onClick={() => setCompanyViewModelState(true)}>Show Company Details</button>
+            
+                
+                <button className="btn btn-info ml-4 mt-1" style={{
+                    fontSize: "1.2em", fontWeight: "700", color: "darkblue",
+                }} data-bs-toggle="button" onClick={() => setCompanyViewModelState(true)}>Show Company Details</button>
 
-            {"guest" === userType && (
-                <button style={{
-                fontSize: "1.2em", fontWeight: "700", color: "darkblue",}}
-                    value={`/worker/application/${company.name}`}
-                    onClick={redirectHandler}
-                    className="btn btn-warning mx-3"
-                >
-                    Apply
-                </button>
-            )}
-            {"customer" === userType && (
-                <>
-                <button style={{
-                fontSize: "1.2em", fontWeight: "700", color: "darkblue",}}
-                    value={`/customer/placeorder/${company.name}`}
-                    onClick={redirectHandler}
-                    className="btn btn-warning mx-3"
-                >
-                    Place Order
-                </button>
-                <button style={{
-                fontSize: "1.2em", fontWeight: "700", color: "lightblue",}}
-                    value={`/customer/subscription-from/${company.name}`}
-                    onClick={redirectHandler}
-                    className="btn btn-danger mx-3"
-                >
-                    Take Subscription
-                </button>
+                {"guest" === userType && (
+                    <button style={{
+                    fontSize: "1.2em", fontWeight: "700", color: "darkblue",}}
+                        value={`/worker/application/${company.name}`}
+                        onClick={redirectHandler}
+                        className="btn btn-warning mx-3"
+                    >
+                        Apply
+                    </button>
+                    
+                )}
+                {"customer" === userType && (
+                    
+                    <>
+                    
+                    <button style={{
+                    fontSize: "1.2em", fontWeight: "700", color: "darkblue",}}
+                        value={`/customer/placeorder/${company.name}`}
+                        onClick={redirectHandler}
+                        className="btn btn-warning mx-3"
+                    >
+                        Place Order
+                    </button>
+                    
+                    
+                    <button style={{
+                    fontSize: "1.2em", fontWeight: "700", color: "lightblue",}}
+                        value={`/customer/subscription-from/${company.name}`}
+                        onClick={redirectHandler}
+                        className="btn btn-danger mx-3"
+                    >
+                        Take Subscription
+                    </button>
+                    
                 </>
-            )}
-        </div>
+                
+                )}
+            </div>
+            </tr>
+            </table>
+            
     );
 }
 
